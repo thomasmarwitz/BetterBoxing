e63d3544dec4a723d28f60b434cbb20bdf110b4b
thomasmarwitz <thomasmarwitz3@gmail.com>
2022-06-22 20:59:36 +0200

    Add mocked python backend version

--- box-scanner/main.py
+++ box-scanner/main.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
-
 import flask
-import json
-from package.scanner import BoxScanner
+from random import choice
+import time
 
 # Create the application.
 APP = flask.Flask(__name__)
 
 @APP.route('/scan_single_box')
 def index():
-    box = BoxScanner().scan_box()
+    box = choice([
+        [100, 30, 500],
+        [30, 50, 70],
+        [23, 76, 99]
+    ])
     response = flask.jsonify(box)
     response.headers.add('Access-Control-Allow-Origin', '*')
+    time.sleep(choice([0.1, 0.5, 1]))
     return response
 
 
